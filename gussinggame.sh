d96acc1322fa60c4428f9b7f65f063f2fec93853
rahulpatidar045 <rjpatidar045@gmail.com>
2020-05-24 22:55:52 +0530

    adding files

--- gussinggame.sh
+++ gussinggame.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 function guess(){
-    true_ans=$(ls -l |grep "^-"|wc -l)
+    ans=$(ls -l |grep "^-"|wc -l)
     while true;
     do
-        echo "pleas enter your guess"
-        read  number
-        if [ $number -lt $true_ans ]
+        echo "try to guess"
+        read  n
+        if [ $n -lt $ans ]
         then
             echo "your guess is Less then the true number"
-        elif [ $number -gt $true_ans ]
+        elif [ $n -gt $ans ]
         then
             echo "your guess is Greater then the true number"
         else
